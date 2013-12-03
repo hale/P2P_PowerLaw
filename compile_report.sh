c56d2384d72f27e5e2e78bb32e35a3ed88bc93de
Philip Hale <phil989@gmail.com>
2013-12-03 05:15:15 +0000

    Assessment finished; submitted.  Simulation runs and report complete.

--- compile_report.sh
+++ compile_report.sh
@@ -2,12 +2,12 @@
 cd doc
 
 auml-new.pl protocol_connect
-auml-new.pl protocol_send_file_list
+auml-new.pl protocol_send_stats
 auml-new.pl protocol_file_search
 auml-new.pl protocol_acquire_file
 
 wish protocol_connect.tk
-wish protocol_send_file_list.tk
+wish protocol_send_stats.tk
 wish protocol_file_search.tk
 wish protocol_acquire_file.tk
 
